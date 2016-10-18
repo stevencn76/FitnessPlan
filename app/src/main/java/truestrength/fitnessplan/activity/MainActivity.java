a9c87582ebc063d79e9977c7ab029d1f89d497e3
stevencn76 <stevencn76@outlook.com>
2016-10-18 14:19:31 +1300

    update

---
+++ app/src/main/java/truestrength/fitnessplan/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package truestrength.fitnessplan.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import truestrength.fitnessplan.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/truestrength/fitnessplan/activity/SplashActivity.java
+++ app/src/main/java/truestrength/fitnessplan/activity/SplashActivity.java
@@ -1,13 +1,15 @@
-package truestrength.fitnessplan;
+package truestrength.fitnessplan.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import truestrength.fitnessplan.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
