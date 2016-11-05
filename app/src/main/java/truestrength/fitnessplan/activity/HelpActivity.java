46d39dbb038c645185a3494e732e1ab91e1a9ad1
stevencn76 <stevencn76@outlook.com>
2016-11-05 15:18:22 +1300

    developing

--- app/src/main/java/truestrength/fitnessplan/activity/HelpActivity.java
+++ app/src/main/java/truestrength/fitnessplan/activity/HelpActivity.java
@@ -3,16 +3,22 @@ package truestrength.fitnessplan.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 import truestrength.fitnessplan.R;
 
 public class HelpActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        webView = (WebView)findViewById(R.id.webViewer);
+        webView.loadUrl("file:///android_res/raw/help.html");
+
+        setTitle("Help");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
